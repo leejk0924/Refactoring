ce1023b94d02b7e653cc99454a78696819d78447
jk <akdirdl@gmail.com>
2023-11-29 23:53:46 +0900

    적립 포인트 계산 코드 추출

--- chapter1/Theater.js
+++ chapter1/Theater.js
@@ -13,15 +13,11 @@ function statement(invoice, plays){
     }).format;
     for(let perf of invoice.performances) {
         // const play = playFor(perf)
-        let thisAmount = 0;
-        thisAmount = amountFor(perf);
-        // 포인트 적립
-        volumeCredits += Math.max(perf.audience-30, 0);
-        if("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        volumeCredits += volumeCreditsFor(perf);
 
         // 청구 내역을 출력
-        result += `${playFor(perf)}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
-        totalAmount += thisAmount;
+        result += `${playFor(perf)}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+        totalAmount += amountFor(perf);
     }
     result += `총액: ${format(totalAmount / 100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
@@ -46,6 +42,15 @@ function amountFor(aPerformance) {
             throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
     }
     return result;
+}
+
+function volumeCreditsFor(aPerformance) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type) {
+        volumeCredits += Math.floor(aPerformance.audience / 5);
+    }
+    return volumeCredits;
 }
 
 function playFor(aPerformance) {

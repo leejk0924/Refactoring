ee49efdce60977f88fff362f6e4a0674cd90d2cf
jk <akdirdl@gmail.com>
2023-11-30 00:17:54 +0900

    format 변수 제거하기

--- chapter1/Theater.js
+++ chapter1/Theater.js
@@ -6,23 +6,27 @@ function statement(invoice, plays){
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내영 (고객명: ${invoice.customer})\n`;
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2,
-    }).format;
+
     for(let perf of invoice.performances) {
         // const play = playFor(perf)
         volumeCredits += volumeCreditsFor(perf);
 
         // 청구 내역을 출력
-        result += `${playFor(perf)}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+        result += `${playFor(perf)}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `총액: ${format(totalAmount / 100)}\n`;
+    result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
 }
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency", currency: "USD",
+        minimumFractionDigits: 2
+    }).format(aNumber/100);
+}
+
 function amountFor(aPerformance) {
     let result = 0;
     switch (playFor(aPerformance).type) {
